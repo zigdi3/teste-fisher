7103baaf8222f2bf41aa255b6bf044bd82616dc8
Diego Araujo <diego.ie.araujo@gmail.com>
2022-05-02 21:14:17 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+
+export class AppComponent implements OnInit {
+
+  itemForm: FormGroup;
+
+  list = [
+    { id: 1, name: "a" },
+    { id: 2, name: "b" },
+    { id: 3, name: "c" },
+    { id: 4, name: "d" },
+    { id: 5, name: "e" },
+  ];
+
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+
+  ngOnInit() {
+    this.itemForm = this.fb.group({
+      item: [null]
+    });
+  }
+
+  submit() { }
+}
